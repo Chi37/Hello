9b32664ba4f5a4fb15ff02e0207a63363d9cf758
Chi <bechi1994@gmail.com>
2018-10-18 14:39:32 -0500

    Added an input method and return an answer with conditionals

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,10 @@
 def sayHello():
 	print("Hello,World, I am using iPython!")
 
+coffee = input("How many cups of coffee have you consumed?")
+if int(coffee) > 4:
+	print("You have a coffee problem")
+else:
+	print("You don't have a problem")
+
+
